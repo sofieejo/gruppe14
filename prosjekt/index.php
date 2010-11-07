020bd256f7f5a6b1a0bd4def3c83cab7fadba09d
Bengi <blackbondi@msn.com>
2010-11-07 22:46:44 +0100

    dagbok, egenvurdering,begrunnelse for designvalg,skisse av nettsiden og updated selve nettsiden av prosjektet

--- prosjekt/index.php
+++ prosjekt/index.php
@@ -2,42 +2,19 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=windows-1252"/>
-		<title>web-prosjekt</title>
+		<title>web-prosjekt gruppe 14</title>
 		<link href="layout.css" rel="stylesheet" type="text/css">
-	</head>
-	
+	</head>	
 	<body>
 		<div id="xinner">
 			<div id="header">
-				<br>
+			</div>
+			<div id ="menu">
 				<a href="?navId=1">Home</a>&nbsp;&nbsp;|&nbsp;&nbsp;
 				<a href="?navId=2">Biografie</a>&nbsp;&nbsp;|&nbsp;&nbsp;
 				<a href="?navId=3">WETA</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-				<a class="popup" href="">Filmliste</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-					<div class="popup">
-						<ul>
-							<li><a href="?navId=4">The Hobbit: Part 1</a></li>
-							<li><a href="?navId=5">The Hobbit: Part 2</a></li>
-							<li><a href="?navId=6">The Lovely Bones</a></li>
-							<li><a href="?navId=7">Crossin the Line</a></li>
-							<li><a href="?navId=8"></a>King Kong</li>
-							<li><a href="?navId=9"></a>The Lost Spider Pit Sequence</li>
-							<li><a href="?navId=10"></a>The Lord of the Rings Trilogy</li>
-							<li><a href="?navId=11">The Frighteners</a></li>
-							<li><a href="?navId=12">Forgotten Silver</a></li>
-							<li><a href="?navId=13">Heavently Creatures</a></li>
-							<li><a href="?navId=14">Braindead</a></li>
-							<li><a href="?navId=15">Meet the Feebles</a></li>
-							<li><a href="?navId=16">Bad Taste</a></li>
-							<li><a href="?navId=17">The Valley</a></li>
-							<li><a href="?navId="></a></li>
-						</ul>
-					</div>
-				<a href="mailto:blackbondi@msn.com">Kontakt</a>
-			</div>
-			<h1>blablabla</h1>
-			<br/>
-			
+				<a href="?navId=4">Filmliste</a>
+				</div>
 			<?php
 				switch($_GET["navId"]) 
 				{
@@ -49,21 +26,34 @@
 					break;
 				case 3:
 					include("weta.html");
-					break;					
+					break;
+				case 4:
+					include("filmer.html");
+					break;
+				case 5:
+					include("kilder.html");
+					break;	
+				case 6:
+					include("sitemap.html");
+					break;	
+				case 7:
+					include("validator.html");
+					break;
+					
 				default:
 					include("home.html");
 					break;
 				}
-			?>
-			
-			<br/><br/>	
+			?>	
 			<div id="footer">
-				<a href="">Prosjektsiden</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-				<a href="">Om oss</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-				<a href="">Site-map</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-				<a href="">Validator</a>
+				<a href="http://www.stud.hio.no/~s163468/Gruppe14/index.html"target="_blank">Prosjektsiden</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+				<a href="?navId=5">Kilder</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+				<a href="?navId=6">Site-map</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+				<a href="?navId=7">Validator</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+				<a href="mailto:blackbondi@msn.com">Kontakt</a>
 			</div>		
-		</div>
-
+		</div><br>
+		<img src="images/html5.jpg" alt="Valid HTML 5">&nbsp;&nbsp;&nbsp;
+		<img src="images/css.jpg" alt="Valid CSS 2.1">
 	</body>
 </html>
